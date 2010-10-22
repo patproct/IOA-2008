5e0743a4cc1119019075903dacff7bfc654e16c2
Patrick Proctor <patrick42h@gmail.com>
2010-10-22 11:17:16 -0400

    =Added subject line to email link

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 						<a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/footer-logo.jpg" alt="IOA: Racing outboards 50 years" border="0" height="90" width="90"></a>
 					</div><!-- end footerlogo div -->
 					<h2>Indiana Outboard Association: <em>We race boats!</em></h2>
-					<p>Materials at this web site are the property of members of the Indiana Outboard Association. Ask before you use, please. Comments and questions may be directed to <a href="mailto:patrick42h@indianaoutboard.org">Patrick Proctor</a>.</p>
+					<p>Materials at this web site are the property of members of the Indiana Outboard Association. Ask before you use, please. Comments and questions may be directed to <a href="mailto:patrick42h@indianaoutboard.org?subject=Comment or question about the IOA website">Patrick Proctor</a>.</p>
 					&nbsp;<br>
 				</td>
 			</tr><!-- end footer row -->
